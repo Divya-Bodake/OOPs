af7efbac1626e3d7513955215c967b0fcc0afbca
Divya-Bodake <divyabodake25@gmail.com>
2022-11-15 01:43:39 +0530

    [Divya]Add:Inventory Data Management

---
+++ OOps/InventoryDataManagement/InventoryDetailsManagementList.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using OOps.InventoryManagement;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOps.InventoryDataManagement
+{
+    internal class InventoryDetailsManagementList
+    {
+        public void ReadJsonFile(string file)
+        {
+            var jsonData = File.ReadAllText(file);
+            InventoryData inventories = JsonConvert.DeserializeObject<InventoryData>(jsonData);
+            List<Inventory> RiceList = inventories.Rice;
+            Display(RiceList);
+            List<Inventory> PulsesList = inventories.Pulses;
+            Display(RiceList);
+            List<Inventory> WheatList = inventories.Wheat;
+            Display(RiceList);
+
+        }
+        public void Display(List<Inventory> inventories)
+        {
+            foreach (var data in inventories)
+            {
+                Console.WriteLine(data.Name + " \n" + data.Weight + " \n" + data.Price);
+            }
+        }
+    }
+}
--- OOps/Program.cs
+++ OOps/Program.cs
@@ -1,15 +1,21 @@
-﻿using OOps.InventoryManagement;
+﻿using OOps.InventoryDataManagement;
+using OOps.InventoryManagement;
 
 namespace OOps
 {
     internal class Program
     {
-        static string inventoryFilePath = @"C:\Bridgelabz\Assignments\Day11-12\OOPs\OOps\Inventory.json";
+        //static string inventoryFilePath = @"C:\Bridgelabz\Assignments\Day11-12\OOPs\OOps\Inventory.json";
+        static string inventoryDataFilePath = @"C:\Bridgelabz\Assignments\Day11-12\OOPs\OOps\InventoryDataManagement\InventoryData.json";
+
 
         static void Main(string[] args)
         {
-            InventoryDetailsManagement inventoryDetailsManagement = new InventoryDetailsManagement();
-            inventoryDetailsManagement.ReadJsonFile(inventoryFilePath);
+            //InventoryDetailsManagement inventoryDetailsManagement = new InventoryDetailsManagement();
+            //inventoryDetailsManagement.ReadJsonFile(inventoryFilePath);
+
+            InventoryDetailsManagementList inventoryDetailsManagementList = new InventoryDetailsManagementList();
+            inventoryDetailsManagementList.ReadJsonFile(inventoryDataFilePath);
         }
     }
 }
